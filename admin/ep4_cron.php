511ef8fe1626db102aad9e5fb73a02846043e817
mc12345678 <mc12345678@users.noreply.github.com>
2020-01-25 09:38:37 -0500

    Do not attempt to redefine a constant that already was defined.
    This is to reduce cron job notifications.

--- admin/ep4_cron.php
+++ admin/ep4_cron.php
@@ -85,8 +85,8 @@ if (!(defined('FILENAME_EASYPOPULATE_4') && file_exists(DIR_FS_ADMIN . (!strstr(
   $csv_delimiter = ","; // "\t" = tab AND "," = COMMA
   $csv_enclosure = '"';
   $category_delimiter = "\x5e";
-  define(EP4_DB_FILTER_KEY, 'products_model');
-  define('EP4_ADMIN_TEMP_DIRECTORY', 'true');
+  if (!defined('EP4_DB_FILTER_KEY')) define(EP4_DB_FILTER_KEY, 'products_model');
+  if (!defined('EP4_ADMIN_TEMP_DIRECTORY')) define('EP4_ADMIN_TEMP_DIRECTORY', 'true');
 
   $tempdir = EASYPOPULATE_4_CONFIG_TEMP_DIR; // This ideally should not actually include the Admin Directory in the variable.
   $ep_date_format = EASYPOPULATE_4_CONFIG_FILE_DATE_FORMAT;
