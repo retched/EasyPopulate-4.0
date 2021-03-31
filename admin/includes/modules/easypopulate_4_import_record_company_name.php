3a749542d58a188abbd4ef9c16edfd037a7fcc77
mc12345678 <mc12345678@users.noreply.github.com>
2021-03-30 22:34:29 -0400

    Rework record company name import
    Use alternate method of admin sanitization
    Refactor code for alignment
    Refactor code to support expanding the size of the field as necessary and
    to then store that new data instead of potentially losing it because the
    field data was too wide.
    Support possibility of sanitizing on stringIgnoreNull

--- admin/includes/modules/easypopulate_4_import_record_company_name.php
+++ admin/includes/modules/easypopulate_4_import_record_company_name.php
@@ -5,187 +5,170 @@
  **/
 
 
-          if (isset($v_record_company_name) && ($v_record_company_name != '')) {
-            if (!empty($_POST)) {
-              $oldRecComp = $_POST;
-              unset($_POST);
-            }
-          
-            if (ep4_field_in_file('v_record_company_name')) {
-              $_POST['record_company_name'] = ep_4_curly_quotes($v_record_company_name);
-            }
-            
-            if (ep4_field_in_file('v_record_company_image')) {
-              $_POST['record_company_image'] = ep_4_curly_quotes($v_record_company_image);
-            }
-            
-            if (class_exists('AdminRequestSanitizer')) {
-              $sanitizer = AdminRequestSanitizer::getInstance();
-              $sanitizer->runSanitizers();
-              unset($sanitizer);
-            }
-          
-            $v_record_company_name = $_POST['record_company_name'];
-            
-            $v_record_company_image = $_POST['record_company_image'];
-            
-            unset($_POST);
-            if (!empty($oldRecComp)) {
-              $_POST = $oldRecComp;
-              unset($oldRecComp);
-            }
-          }
-          $record_company_name_str_len = isset($v_record_company_name) && ($v_record_company_name != '') ? (function_exists('mb_strlen') ? mb_strlen($v_record_company_name) : strlen($v_record_company_name)) : false;
-          if ($record_company_name_str_len === false) {
-            $display_output .= sprintf(EASYPOPULATE_4_DISPLAY_RESULT_RECORD_COMPANY_NAME_EMPTY, $items[$filelayout[$chosen_key]], $chosen_key);
-            $v_record_company_id = 0; // chadd - zencart uses artists_id = '0' for no assisgned artists
-            return;
-          }
-          if ($record_company_name_str_len <= $max_len['record_company_name']) {
-            $sql = "SELECT record_company_id AS record_companyID FROM " . TABLE_RECORD_COMPANY . " WHERE record_company_name = :record_company_name: LIMIT 1";
-            $sql = $db->bindVars($sql, ':record_company_name:', ep_4_curly_quotes($v_record_company_name), $zc_support_ignore_null);
-            $result = ep_4_query($sql);
-            if ($row = ($ep_uses_mysqli ? mysqli_fetch_array($result) : mysql_fetch_array($result) )) {
-              $v_record_company_id = $row['record_companyID']; // this id goes into the product_music_extra table
-              $sql = "UPDATE " . TABLE_RECORD_COMPANY . " SET
-                record_company_image = :record_company_image:,
-                last_modified = CURRENT_TIMESTAMP
-                WHERE record_company_id = :record_company_id:";
-              $sql = $db->bindVars($sql, ':record_company_image:', $v_record_company_image, $zc_support_ignore_null);
-              $sql = $db->bindVars($sql, ':record_company_id:', $v_record_company_id, 'integer');
-              $result = ep_4_query($sql);
-              if ($result) {
-                zen_record_admin_activity('Updated record company ' . (int) $v_record_company_id . ' via EP4.', 'info');
-              }
-              $v_record_company_url = array();
-              
-              foreach ($langcode as $lang) {
-                $l_id = $lang['id'];
-                $l_id_code = $lang['code'];
-                if (!isset($filelayout['v_record_company_url_' . $l_id]) && !isset($filelayout['v_record_company_url_' . $l_id_code])) {
-                  unset($l_id);
-                  unset($l_id_code);
-                  unset($lang);
-                  return;
-                }
-                
-                if (ep4_field_in_file('v_record_company_url_' . $l_id)) {
-                  $v_record_company_url_store = $v_record_company_url[$l_id] = $items[$filelayout['v_record_company_url_' . $l_id]];
-                }
-                if (ep4_field_in_file('v_record_company_url_' . $l_id_code)) {
-                  $v_record_company_url[$l_id_code] = $items[$filelayout['v_record_company_url_' . $l_id_code]];
-                  if (EASYPOPULATE_4_CONFIG_IMPORT_OVERRIDE == 'language_code' || !ep4_field_in_file('v_record_company_url_' . $l_id)) {
-                    $v_record_company_url_store = $v_record_company_url[$l_id_code];
-                  }
-                  // Prioritize that if the $lang_id is also present for this language that this content rules/overrides.
-                }
-                
-                if (isset($v_record_company_url_store) && ($v_record_company_url_store != '')) {
-                  if (!empty($_POST)) {
-                    $oldRecComUrl = $_POST;
-                    unset($_POST);
-                  }
-                  
-                  if (ep4_field_in_file('v_record_company_url')) {
-                    $_POST['record_company_url'] = ep_4_curly_quotes($v_record_company_url_store);
-                  }
-                  
-                  if (class_exists('AdminRequestSanitizer')) {
-                    $sanitizer = AdminRequestSanitizer::getInstance();
-                    $sanitizer->runSanitizers();
-                    unset($sanitizer);
-                  }
-                  
-                  $v_record_company_url_store = $_POST['record_company_url'];
-                  
-                  unset($_POST);
-                  if (!empty($oldRecComUrl)) {
-                    $_POST = $oldRecComUrl;
-                    unset($oldRecComUrl);
-                  }
-                }
-                
+if (isset($v_record_company_name) && ($v_record_company_name != '')) {
+  $post_array = array(
+    'record_company_name' => array(
+      'v_record_company_name' => compact('v_record_company_name'),
+    ),
+    'record_company_image' => array(
+      'v_record_company_image' => compact('v_record_company_image'),
+    ),
+  );
+  
+  $data_array = ep4_post_sanitize($post_array);
+  extract($data_array, EXTR_OVERWRITE);
+
+
+}
+$record_company_name_str_len = isset($v_record_company_name) && ($v_record_company_name != '') ? (function_exists('mb_strlen') ? mb_strlen($v_record_company_name) : strlen($v_record_company_name)) : false;
+$v_record_company_id = 0; // chadd - zencart uses artists_id = '0' for no assisgned artists
+
+if ($record_company_name_str_len === false) {
+  $display_output .= sprintf(EASYPOPULATE_4_DISPLAY_RESULT_RECORD_COMPANY_NAME_EMPTY, $items[$filelayout[$chosen_key]], $chosen_key);
+  return;
+}
+
+if ($record_company_name_str_len > $max_len['record_company_name']) {
+  $display_output .= sprintf(EASYPOPULATE_4_DISPLAY_RESULT_RECORD_COMPANY_NAME_LONG, $v_record_company_name, $max_len['record_company_name']);
+  if (empty($max_len['record_company_name_found']) || $max_len['record_company_name_found'] < $record_company_name_str_len) {
+    $max_len['record_company_name_found'] = $record_company_name_str_len;
+    if (EASYPOPULATE_4_CONFIG_AUTO_EXTEND_FIELD === 'false') {
+      $ep_error_count++;
+      unset($record_company_name_str_len);
+      return;
+    }
+    $update_record_company_name_sql = "ALTER TABLE " . TABLE_RECORD_COMPANY . " CHANGE record_company_name record_company_name VARCHAR(" . (int)$record_company_name_str_len . ") NOT NULL DEFAULT '';";
+    $update_record_company_name = $db->Execute($update_record_company_name_sql);
+
+    zen_record_admin_activity('Extended table ' . TABLE_RECORD_COMPANY . ' field record_company_name via EP4 from ' . zen_db_input($max_len['record_company_name']) . ' to ' . zen_db_input($record_company_name_str_len) . '.', 'info');
+
+    $max_len['record_company_name'] = $record_company_name_str_len;
+  }
+}
+unset($record_company_name_str_len);
+$sql = "SELECT record_company_id AS record_companyID FROM " . TABLE_RECORD_COMPANY . " WHERE record_company_name = :record_company_name: LIMIT 1";
+$sql = $db->bindVars($sql, ':record_company_name:', ep_4_curly_quotes($v_record_company_name), $zc_support_ignore_null);
+$result = ep_4_query($sql);
+if ($row = ($ep_uses_mysqli ? mysqli_fetch_array($result) : mysql_fetch_array($result) )) {
+  $v_record_company_id = $row['record_companyID']; // this id goes into the product_music_extra table
+  $sql = "UPDATE " . TABLE_RECORD_COMPANY . " SET
+    record_company_image = :record_company_image:,
+    last_modified = CURRENT_TIMESTAMP
+    WHERE record_company_id = :record_company_id:";
+  $sql = $db->bindVars($sql, ':record_company_image:', $v_record_company_image, $zc_support_ignore_null);
+  $sql = $db->bindVars($sql, ':record_company_id:', $v_record_company_id, 'integer');
+  $result = ep_4_query($sql);
+  if ($result) {
+    zen_record_admin_activity('Updated record company ' . (int) $v_record_company_id . ' via EP4.', 'info');
+  }
+  $v_record_company_url = array();
+  
+  foreach ($langcode as $lang) {
+    $l_id = $lang['id'];
+    $l_id_code = $lang['code'];
+    if (!isset($filelayout['v_record_company_url_' . $l_id]) && !isset($filelayout['v_record_company_url_' . $l_id_code])) {
+      unset($l_id);
+      unset($l_id_code);
+      unset($lang);
+      return;
+    }
+    
+    if (ep4_field_in_file('v_record_company_url_' . $l_id)) {
+      $v_record_company_url_store = $v_record_company_url[$l_id] = $items[$filelayout['v_record_company_url_' . $l_id]];
+    }
+    if (ep4_field_in_file('v_record_company_url_' . $l_id_code)) {
+      $v_record_company_url[$l_id_code] = $items[$filelayout['v_record_company_url_' . $l_id_code]];
+      if (EASYPOPULATE_4_CONFIG_IMPORT_OVERRIDE == 'language_code' || !ep4_field_in_file('v_record_company_url_' . $l_id)) {
+        $v_record_company_url_store = $v_record_company_url[$l_id_code];
+      }
+      // Prioritize that if the $lang_id is also present for this language that this content rules/overrides.
+    }
+    
+    if (isset($v_record_company_url_store) && ($v_record_company_url_store != '')) {
+
+      $post_array = array(
+        'record_company_url' => array(
+          'v_record_company_url' => compact('v_record_company_url_store'),
+        ),
+      );
+
+      $data_array = ep4_post_sanitize($post_array);
+      
+      extract($data_array, EXTR_OVERWRITE);
+      
+    }
+    
 /*                // Ensure that $lang_id version of variable is populated for other uses.
                 if (!ep4_field_in_file('v_record_company_url_' . $l_id) && ep4_field_in_file('v_record_company_url_' . $l_id_code)) {
                   $v_record_company_url[$l_id] = $v_record_company_url[$l_id_code];
                 }
 */                
-                $sql = "UPDATE " . TABLE_RECORD_COMPANY_INFO . " SET
-                  record_company_url = :record_company_url:
-                  WHERE record_company_id = :record_company_id: AND languages_id = :languages_id:";
-                $sql = $db->bindVars($sql, ':record_company_url:', $v_record_company_url_store, $zc_support_ignore_null);
-                $sql = $db->bindVars($sql, ':record_company_id:', $v_record_company_id, 'integer');
-                $sql = $db->bindVars($sql, ':languages_id:', $l_id, 'integer');
-                $result = ep_4_query($sql);
-                if ($result) {
-                  zen_record_admin_activity('Updated record company info ' . (int) $v_record_company_id . ' via EP4.', 'info');
-                }
-              }
-              unset($lang);
-            } else { // It is set to autoincrement, do not need to fetch max id
-              // When inserting a new record company, need to also ensure insertion
-              //   of matching record company information records for each language.
-              //   This at least matches ZC operation.
-              $sql = "INSERT INTO " . TABLE_RECORD_COMPANY . " (record_company_name, record_company_image, date_added)
-                VALUES (:record_company_name:, :record_company_image:, CURRENT_TIMESTAMP)";
-              $sql = $db->bindVars($sql, ':record_company_name:', ep_4_curly_quotes($v_record_company_name), $zc_support_ignore_null);
-              $sql = $db->bindVars($sql, ':record_company_image:', $v_record_company_image, $zc_support_ignore_null);
-              $result = ep_4_query($sql);
+    $sql = "UPDATE " . TABLE_RECORD_COMPANY_INFO . " SET
+      record_company_url = :record_company_url:
+      WHERE record_company_id = :record_company_id: AND languages_id = :languages_id:";
+    $sql = $db->bindVars($sql, ':record_company_url:', $v_record_company_url_store, $zc_support_ignore_null);
+    $sql = $db->bindVars($sql, ':record_company_id:', $v_record_company_id, 'integer');
+    $sql = $db->bindVars($sql, ':languages_id:', $l_id, 'integer');
+    $result = ep_4_query($sql);
+    if ($result) {
+      zen_record_admin_activity('Updated record company info ' . (int) $v_record_company_id . ' via EP4.', 'info');
+    }
+  }
+  unset($lang);
+  return;
+} /*else {*/ // It is set to autoincrement, do not need to fetch max id
+// When inserting a new record company, need to also ensure insertion
+//   of matching record company information records for each language.
+//   This at least matches ZC operation.
+$sql = "INSERT INTO " . TABLE_RECORD_COMPANY . " (record_company_name, record_company_image, date_added)
+  VALUES (:record_company_name:, :record_company_image:, CURRENT_TIMESTAMP)";
+$sql = $db->bindVars($sql, ':record_company_name:', ep_4_curly_quotes($v_record_company_name), $zc_support_ignore_null);
+$sql = $db->bindVars($sql, ':record_company_image:', $v_record_company_image, $zc_support_ignore_null);
+$result = ep_4_query($sql);
 
-              $v_record_company_id = ($ep_uses_mysqli ? mysqli_insert_id($db->link) : mysql_insert_id()); // id is auto_increment, so can use this function
+$v_record_company_id = ($ep_uses_mysqli ? mysqli_insert_id($db->link) : mysql_insert_id()); // id is auto_increment, so can use this function
 
-              if ($result) {
-                zen_record_admin_activity('Inserted record company ' . zen_db_input(ep_4_curly_quotes($v_record_company_name)) . ' via EP4.', 'info');
-              }
+if ($result) {
+  zen_record_admin_activity('Inserted record company ' . zen_db_input(ep_4_curly_quotes($v_record_company_name)) . ' via EP4.', 'info');
+}
 
-              $v_record_company_url = array();
+$v_record_company_url = array();
 
-              foreach ($langcode as $lang) {
-                $l_id = $lang['id'];
-                $l_id_code = $lang['code'];
-                
-                $v_record_company_url = array();
-                
-                if (ep4_field_in_file('v_record_company_url_' . $l_id)) {
-                  $v_record_company_url_store = $v_record_company_url[$l_id] = $items[$filelayout['v_record_company_url_' . $l_id]];
-                }
-                if (ep4_field_in_file('v_record_company_url_' . $l_id_code)) {
-                  $v_record_company_url[$l_id_code] = $items[$filelayout['v_record_company_url_' . $l_id_code]];
-                  if (EASYPOPULATE_4_CONFIG_IMPORT_OVERRIDE == 'language_code' || !ep4_field_in_file('v_record_company_url_' . $l_id)) {
-                    $v_record_company_url_store = $v_record_company_url[$l_id_code];
-                  }
-                  // Prioritize that if the $lang_id is also present for this language that this content rules/overrides.
-                }
-                $sql = "INSERT INTO " . TABLE_RECORD_COMPANY_INFO . " (record_company_id, languages_id, record_company_url)
-                  VALUES (:record_company_id:, :languages_id:, :record_company_url:)"; // seems we are skipping manufacturers url
-                $sql = $db->bindVars($sql, ':record_company_id:', $v_record_company_id, 'integer');
-                $sql = $db->bindVars($sql, ':languages_id:', $l_id, 'integer');
-                $sql = $db->bindVars($sql, ':record_company_url:',  (isset($filelayout['v_record_company_url_' . $l_id]) ? $v_record_company_url_store : (isset($filelayout['v_record_company_url_' . $lid]) ? $items[$filelayout['v_record_company_url_' . $lid]] : '')), $zc_support_ignore_null);
-                $result = ep_4_query($sql);
-                if ($result) {
-                  zen_record_admin_activity('Inserted record company info ' . (int) $v_record_company_id . ' via EP4.', 'info');
-                }
-              }
-              unset($lang);
-            }
-          } else { // $v_record_company_name == '' or name length violation
-            if ($record_company_name_str_len > $max_len['record_company_name']) {
-              $display_output .= sprintf(EASYPOPULATE_4_DISPLAY_RESULT_RECORD_COMPANY_NAME_LONG, $v_record_company_name, $max_len['record_company_name']);
-              if (empty($max_len['record_company_name_found']) || $max_len['record_company_name_found'] < $record_company_name_str_len) {
-                $max_len['record_company_name_found'] = $record_company_name_str_len;
-                if (EASYPOPULATE_4_CONFIG_AUTO_EXTEND_FIELD === 'false') {
-                  $ep_error_count++;
-                  unset($record_company_name_str_len);
-                  return;
-                }
-                $update_record_company_name_sql = "ALTER TABLE " . TABLE_RECORD_COMPANY . " CHANGE record_company_name record_company_name VARCHAR(" . $record_company_name_str_len . ") NOT NULL DEFAULT '';";
-                $update_record_company_name = $db->Execute($update_record_company_name_sql);
+foreach ($langcode as $lang) {
+  $l_id = $lang['id'];
+  $l_id_code = $lang['code'];
+  
+  $v_record_company_url = array();
+  
+  if (ep4_field_in_file('v_record_company_url_' . $l_id)) {
+    $v_record_company_url_store = $v_record_company_url[$l_id] = $items[$filelayout['v_record_company_url_' . $l_id]];
+  }
+  if (ep4_field_in_file('v_record_company_url_' . $l_id_code)) {
+    $v_record_company_url[$l_id_code] = $items[$filelayout['v_record_company_url_' . $l_id_code]];
+    if (EASYPOPULATE_4_CONFIG_IMPORT_OVERRIDE == 'language_code' || !ep4_field_in_file('v_record_company_url_' . $l_id)) {
+      $v_record_company_url_store = $v_record_company_url[$l_id_code];
+    }
+    // Prioritize that if the $lang_id is also present for this language that this content rules/overrides.
+  }
+  
+  $post_array = array(
+    'record_company_url' => array(
+      'v_record_company_url' => compact('v_record_company_url_store'),
+    ),
+  );
 
-                zen_record_admin_activity('Extended table ' . TABLE_RECORD_COMPANY . ' field record_company_name via EP4 from ' . zen_db_input($max_len['record_company_name']) . ' to ' . zen_db_input($record_company_name_str_len) . '.', 'info');
-
-                $max_len['record_company_name'] = $record_company_name_str_len;
-              }
-            }
-            $v_record_company_id = 0; // record_company_id = '0' for no assisgned artists
-          }
-          unset($record_company_name_str_len);
+  $data_array = ep4_post_sanitize($post_array);
+  
+  extract($data_array, EXTR_OVERWRITE);
+  
+  $sql = "INSERT INTO " . TABLE_RECORD_COMPANY_INFO . " (record_company_id, languages_id, record_company_url)
+    VALUES (:record_company_id:, :languages_id:, :record_company_url:)"; // seems we are skipping manufacturers url
+  $sql = $db->bindVars($sql, ':record_company_id:', $v_record_company_id, 'integer');
+  $sql = $db->bindVars($sql, ':languages_id:', $l_id, 'integer');
+  $sql = $db->bindVars($sql, ':record_company_url:',  $v_record_company_url_store, $zc_support_ignore_null);
+  $result = ep_4_query($sql);
+  if ($result) {
+    zen_record_admin_activity('Inserted record company info ' . (int) $v_record_company_id . ' via EP4.', 'info');
+  }
+}
+unset($lang);
