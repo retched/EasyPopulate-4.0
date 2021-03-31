f0230554b0db64e2ca10c828e6898f7985c5607a
mc12345678 <mc12345678@users.noreply.github.com>
2021-03-30 21:58:14 -0400

    Basic sanitize and fix record company name import

--- admin/includes/languages/english/easypopulate_4.php
+++ admin/includes/languages/english/easypopulate_4.php
@@ -31,6 +31,7 @@ define('EASYPOPULATE_4_DISPLAY_RESULT_UPLOAD_EMPTY','<br /><b>Upload button was 
 define('EASYPOPULATE_4_DISPLAY_RESULT_UPLOAD_NO_CSV', ' file is not a CSV file. See below for options.');
 define('EASYPOPULATE_4_DISPLAY_RESULT_ARTISTS_NAME_EMPTY', '<br /><font color="red"><b>SKIPPED! - Artist Name for:</b> %1$s - not provided when using primary Key: %2$s</font>');
 define('EASYPOPULATE_4_DISPLAY_RESULT_ARTISTS_NAME_LONG','<br /><font color="red"><b>SKIPPED! - Artist Name:</b> %1$s - exceeds max. length: %2$s</font>');
+define('EASYPOPULATE_4_DISPLAY_RESULT_RECORD_COMPANY_NAME_EMPTY', '<br /><font color="red"><b>SKIPPED! - Record Company Name for:</b> %1$s - not provided when using primary Key: %2$s</font>');
 define('EASYPOPULATE_4_DISPLAY_RESULT_RECORD_COMPANY_NAME_LONG','<br /><font color="red"><b>SKIPPED! - Record Company Name:</b> %1$s - exceeds max. length: %2$s</font>');
 define('EASYPOPULATE_4_DISPLAY_RESULT_MUSIC_GENRE_NAME_EMPTY', '<br /><font color="red"><b>SKIPPED! - Music Genre Name for:</b> %1$s - not provided when using primary Key: %2$s</font>');
 define('EASYPOPULATE_4_DISPLAY_RESULT_MUSIC_GENRE_NAME_LONG','<br /><font color="red"><b>SKIPPED! - Music Genre Name:</b> %1$s - exceeds max. length: %2$s</font>');
