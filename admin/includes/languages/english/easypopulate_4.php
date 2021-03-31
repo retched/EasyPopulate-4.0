047b72fedd6951554b49ed66b575afa0863df596
mc12345678 <mc12345678@users.noreply.github.com>
2021-03-30 21:57:42 -0400

    Basic sanitize imported music genre name data

--- admin/includes/languages/english/easypopulate_4.php
+++ admin/includes/languages/english/easypopulate_4.php
@@ -32,6 +32,7 @@ define('EASYPOPULATE_4_DISPLAY_RESULT_UPLOAD_NO_CSV', ' file is not a CSV file. 
 define('EASYPOPULATE_4_DISPLAY_RESULT_ARTISTS_NAME_EMPTY', '<br /><font color="red"><b>SKIPPED! - Artist Name for:</b> %1$s - not provided when using primary Key: %2$s</font>');
 define('EASYPOPULATE_4_DISPLAY_RESULT_ARTISTS_NAME_LONG','<br /><font color="red"><b>SKIPPED! - Artist Name:</b> %1$s - exceeds max. length: %2$s</font>');
 define('EASYPOPULATE_4_DISPLAY_RESULT_RECORD_COMPANY_NAME_LONG','<br /><font color="red"><b>SKIPPED! - Record Company Name:</b> %1$s - exceeds max. length: %2$s</font>');
+define('EASYPOPULATE_4_DISPLAY_RESULT_MUSIC_GENRE_NAME_EMPTY', '<br /><font color="red"><b>SKIPPED! - Music Genre Name for:</b> %1$s - not provided when using primary Key: %2$s</font>');
 define('EASYPOPULATE_4_DISPLAY_RESULT_MUSIC_GENRE_NAME_LONG','<br /><font color="red"><b>SKIPPED! - Music Genre Name:</b> %1$s - exceeds max. length: %2$s</font>');
 define('EASYPOPULATE_4_DISPLAY_RESULT_SBA_DETAIL_NOT_FOUND','<br /><font color="red"><b>SKIPPED! - Attribute Entry on %2$s: </b>%1$s - Not Found!</font>');
 define('EASYPOPULATE_4_DISPLAY_RESULT_BASIC_ATTRIB_NEW','<br /><font color="green"><b>NEW ATTRIBUTE! - %1$s:</b> %2$s, <b>Option:</b> %3$s, <b>Values:</b> %4$s</font>');
