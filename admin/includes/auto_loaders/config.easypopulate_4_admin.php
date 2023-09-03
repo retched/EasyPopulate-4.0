a9091b1b8b1749813fed7df8cb624c4af8cfbce3
mc12345678 <mc12345678@users.noreply.github.com>
2023-09-03 14:29:37 -0400

    Add load of EP4 language extra_definitions

---
+++ admin/includes/auto_loaders/config.easypopulate_4_admin.php
@@ -0,0 +1,6 @@
+<?php
+/* loads and initiates language defines within the admin file structure
+*/
+  $autoLoadConfig[68][] = array('autoType'=>'init_script',
+                                'loadFile'=> 'init_easypopulate_4_lang_defines.php');
+
