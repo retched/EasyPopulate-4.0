48f76dac28641c19686b28477073afabc5410513
mc12345678 <mc12345678@users.noreply.github.com>
2024-04-01 00:17:06 -0400

    Update manifest.php

--- zc_plugins/EasyPopulateV4/v4.0.39.ZC/manifest.php
+++ zc_plugins/EasyPopulateV4/v4.0.39.ZC/manifest.php
@@ -1,11 +1,11 @@
 <?php
 return [
     'pluginVersion' => 'v4.0.39.ZC',
-    'pluginName' => 'EasyPopulate V4',
+    'pluginName' => 'EasyPopulate V4 (EP4)',
     'pluginDescription' => 'Means to access database information for product in a mass method rather than manipulating product information one screen at a time.',
-    'pluginAuthor' => 'McNumbers Ware mc12345678',
+    'pluginAuthor' => 'mc12345678 (McNumbers Ware, mc12345678.com) with ChadD as original author',
     'pluginId' => 2069, // ID from Zen Cart forum
-    'zcVersions' => ['v139,v150,v151,v153,v154,v155,v156,v157'],
+    'zcVersions' => ['v139','v150','v151','v153','v154','v155','v156','v157','v158'],
     'changelog' => 'changelog.txt', // online URL (eg github release tag page, or changelog file there) or local filename only, ie: changelog.txt (in same dir as this manifest file)
     'github_repo' => 'https://github.com/mc12345678/EasyPopulate-4.0', // url
     'pluginGroups' => [],
